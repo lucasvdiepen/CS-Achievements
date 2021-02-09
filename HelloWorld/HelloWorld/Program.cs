a667488fc118d27ad2fd056b83c430c37241a69d
Lucas van Diepen <lucascool@ziggo.nl>
2021-02-09 15:54:06 +0100

    Added HelloWorld and Les1TijdOpHetScherm

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            PrintSlow("Hello World!");
+        }
+
+        private static void PrintSlow(string text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                Console.Write(text[i]);
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
---
+++ Les1TijdOpHetScherm/Les1TijdOpHetScherm/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace Les1TijdOpHetScherm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int colorCounter = 0;
+            int colorIndex = 0;
+
+            ConsoleColor[] colors = {ConsoleColor.Red, ConsoleColor.Cyan, ConsoleColor.Blue, ConsoleColor.Green, ConsoleColor.Yellow};
+
+            while(true)
+            {
+                DateTime date = DateTime.Now;
+
+                Console.Clear();
+                Console.WriteLine(date.ToString("HH::mm:ss"));
+
+                Thread.Sleep(1000);
+                colorCounter++;
+                if(colorCounter >= 10)
+                {
+                    colorCounter = 0;
+
+                    Console.ForegroundColor = colors[colorIndex];
+
+                    colorIndex++;
+                    if (colorIndex >= colors.Length) colorIndex = 0;
+                }
+            }
+        }
+    }
+}
