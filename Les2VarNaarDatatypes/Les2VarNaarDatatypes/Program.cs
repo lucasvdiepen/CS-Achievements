70241661256da00d30c809a432c23f736ad24676
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2021-03-09 14:58:57 +0100

    Added Les2VarNaarDatatypes

---
+++ Les2VarNaarDatatypes/Les2VarNaarDatatypes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Les2VarNaarDatatypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int a = 1;
+            float b = 0.5f;
+            double c = 1.32142412441d;
+            string d = "Word";
+            char e = 'c';
+            bool f = true;
+        }
+    }
+}
